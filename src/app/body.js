aaf6218a4d5f0e21de129321707f97e38a51b8d5
UniqueJackal5 <36040284+UniqueJackal5@users.noreply.github.com>
2024-01-24 18:54:12 +0545

    added images and backgroudn

--- src/app/body.js
+++ src/app/body.js
@@ -1,16 +1,27 @@
 
 import Image from 'next/image';
 import './globals.css';
+import bgImg from './bgImg.jpeg';
 
 export default function Body(){
     return (
-        <div classname="container">
+        <div className="container-fluid px-0 py-0">
             <div>
                 <Image
                 className='bgImg'
-                src=""
+                src={bgImg}
+                // height='1080'
+                // width='auto'
                 alt=''
                 />
+            </div>
+            <div className='overlay'>
+
+            </div>
+            <div className='textOverlay'>
+                <h3>
+                    Welcome to LX FIFA Club
+                </h3>
             </div>
 
         </div>
