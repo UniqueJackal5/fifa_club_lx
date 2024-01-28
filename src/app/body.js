6fa2b3c14e9a33fea0bd65b29c000dbaefde69ba
Santosh Adhikari <santoshadhikari486@gmail.com>
2024-01-28 16:07:00 +0300

    working on mobile friendly

--- src/app/body.js
+++ src/app/body.js
@@ -7,22 +7,23 @@ export default function Body(){
     return (
         <div className="container-fluid px-0 py-0">
             <div>
-                <Image
+                {/* <Image
                 className='bgImg'
                 src={bgImg}
                 // height='1080'
                 // width='auto'
                 alt=''
-                />
+                /> */}
             </div>
-            <div className='overlay'>
 
-            </div>
-            <div className='textOverlay'>
+            {/* <div className='overlay'>
+
+            </div> */}
+            {/* <div className='textOverlay'>
                 <h3>
                     Welcome to LX FIFA Club
                 </h3>
-            </div>
+            </div> */}
 
         </div>
     );
